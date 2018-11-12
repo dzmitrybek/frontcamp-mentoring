864417f99ab1ff180d6667ee7949bc5eb09b5d0f
Dzmitry_Bek@epam.com <Dzmitry_Bek@epam.com>
2018-11-13 00:28:43 +0300

    fix for github pages

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,15 @@
 export const CONSTANTS = {
     API_KEY: '0705e7dcd0114afdb2e897d0635afc29',
+    BASE_URL: window.location.href,
     get DEFAULT_NEWS_URL() {
         return `https://newsapi.org/v2/top-headlines?country=us&apiKey=${this.API_KEY}`
+    },
+    get NO_IMAGE_URL() {
+        return `${this.BASE_URL}/assets/no-image-box.png`;
     },
     GET_QUERY_NEWS_URL(query) {
         return `https://newsapi.org/v2/everything?q=${query}&apiKey=${this.API_KEY}`
     },
-    NO_IMAGE_URL: '/assets/no-image-box.png',
     EMPTY_VALUE: '',
     NO_SEARCH_RESULT_TEXT: 'No Search Results',
     ERROR_TEXT: 'Data Loading Error',
