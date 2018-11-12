23385b9e184ae622df9d9fc3a7ef82bbac52db4c
Dzmitry_Bek@epam.com <Dzmitry_Bek@epam.com>
2018-11-13 00:22:04 +0300

    refactoring

--- src/app/AppModel.js
+++ src/app/AppModel.js
@@ -1,18 +1,11 @@
 import { CONSTANTS } from 'constants.js';
 
 export default class AppModel {
-    constructor() {
-        this.news = [];
-        this.appContentElement = document.querySelector('#app-content');
-    }
+    constructor() {}
 
     getNews(query) {
-        return this.news.lenght ? this.news : this.loadNews(query);
-    }
-
-    loadNews(query) {
         return fetch(query ? CONSTANTS.GET_QUERY_NEWS_URL(query) : CONSTANTS.DEFAULT_NEWS_URL)
             .then((response) => response.json())
-            .then((data) => this.news = data.articles)
+            .then((data) => data.articles)
     }
 }
