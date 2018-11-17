d1ff68631bbb6538191fa94bc68c7042b52c8e80
Dzmitry_Bek@epam.com <Dzmitry_Bek@epam.com>
2018-11-18 01:32:13 +0300

    add babel-polyfill and partial async/await feature

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 import AppController from 'app/AppController';
 import AppView from 'app/AppView';
 import AppModel from 'app/AppModel';
--- src/app/AppModel.js
+++ src/app/AppModel.js
@@ -3,9 +3,9 @@ import { CONSTANTS } from 'constants.js';
 export default class AppModel {
     constructor() {}
 
-    getNews(query) {
-        return fetch(query ? CONSTANTS.GET_QUERY_NEWS_URL(query) : CONSTANTS.DEFAULT_NEWS_URL)
-            .then((response) => response.json())
-            .then((data) => data.articles)
+    async getNews(query) {
+        const response = await fetch(query ? CONSTANTS.GET_QUERY_NEWS_URL(query) : CONSTANTS.DEFAULT_NEWS_URL);
+        const data = await response.json();
+        return data.articles;
     }
 }
